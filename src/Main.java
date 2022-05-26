1d5186bb37e7a72b00c162354c3e170b493dbef9
Mayank.Verma <mayank.verma@1mg.com>
2022-05-26 19:58:29 +0530

    Add Scanner input

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,14 @@ public class Main {
         double studentGPA = 3.45;
         String studentFirstName = "Kayla";
         String studentLastName = "Hammon";
+        String space = " ";
         System.out.println(studentFirstName + " " +
                 studentLastName + " has a GPA of " + studentGPA);
         System.out.println("What do you want to update it to?");
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+        System.out.println(studentFirstName + space + studentLastName + space + "has the updated GPA: "+studentGPA);
+
     }
 }
