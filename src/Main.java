5248c4ba52c5f70ec56f8190a165f9a19ba57419
Mayank.Verma <mayank.verma@1mg.com>
2022-05-26 20:31:54 +0530

    Move scanner above print

--- src/Main.java
+++ src/Main.java
@@ -15,15 +15,15 @@ public class Main {
                 studentLastName + " has a GPA of " + studentGPA);
         System.out.println("What do you want to update it to?");
 
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+        System.out.println(studentFirstName + space + studentLastName + space + "has the updated GPA: "+studentGPA);
+
         System.out.println(studentFirstName);
         System.out.println(studentLastName);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
         System.out.println("Student name is: "+ studentFirstName + " " + studentLastName);
-
-        Scanner input = new Scanner(System.in);
-        studentGPA = input.nextDouble();
-        System.out.println(studentFirstName + space + studentLastName + space + "has the updated GPA: "+studentGPA);
     }
 }
